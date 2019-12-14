2f67d5151d6dd28b7b7a41bc39397b7643dbe9e1
Bonkook Koo <djmimi@nate.com>
2019-12-14 19:17:25 +0900

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom';
 import About from './pages/About';
+import NoticeList from './pages/notice/NoticeList';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <About />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/notices">
+          <NoticeList />
+        </Route>
+        <Route path="/">
+          <Redirect to="about" />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
