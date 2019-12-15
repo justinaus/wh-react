338bb40459cb8a2fc63e05b41ead980772804644
Bonkook Koo <djmimi@nate.com>
2019-12-15 12:11:49 +0900

    Update page pagelayout

--- src/pages/notice/NoticeList.tsx
+++ src/pages/notice/NoticeList.tsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import PageLayout from '../../components/layout/PageLayout';
 
 export default class NoticeList extends Component {
   render() {
-    return <div>NoticeList</div>;
+    return <PageLayout>NoticeList</PageLayout>;
   }
 }
