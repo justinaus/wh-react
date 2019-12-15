e3f27029031a5139bc4561118896c76b31c810b1
Bonkook Koo <djmimi@nate.com>
2019-12-15 12:08:42 +0900

    Update nav bar

--- src/components/layout/header/NavBar.tsx
+++ src/components/layout/header/NavBar.tsx
@@ -2,34 +2,39 @@ import React, { Component } from 'react';
 
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { menuData } from '../../../constants/menuData';
+import IMenuItemData from '../../../constants/IMenuItemData';
 
 export default class NavBar extends Component {
-  numbers = [1, 2, 3, 4, 5];
+  getDropDown = (menuItemData: IMenuItemData) => (
+    <NavDropdown
+      key={menuItemData.id}
+      title={menuItemData.text}
+      id="basic-nav-dropdown"
+    >
+      {this.getLinkElement(menuItemData.arrSub!, 'dropdown-item')}
+    </NavDropdown>
+  );
 
-  listItems = this.numbers.map(number => (
-    <li key={number.toString()}>{number}</li>
-  ));
+  getLinkElement = (arrMenu: Array<IMenuItemData>, className = 'nav-link') => {
+    return arrMenu.map(item => {
+      if (item.arrSub) {
+        return this.getDropDown(item);
+      }
+
+      return (
+        <Link key={item.id} to={item.link} className={className}>
+          {item.text}
+        </Link>
+      );
+    });
+  };
 
   render() {
     return (
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="/">Home</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Link to="/about" className="nav-link">
-            About
-          </Link>
-          <Link to="/notices" className="nav-link">
-            Notice
-          </Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <Link to="/about" className="dropdown-item">
-              About
-            </Link>
-            <Link to="/notices" className="dropdown-item">
-              Notice
-            </Link>
-          </NavDropdown>
-        </Nav>
+        <Nav className="mr-auto">{this.getLinkElement(menuData)}</Nav>
       </Navbar>
     );
   }
