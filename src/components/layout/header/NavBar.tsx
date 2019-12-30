e7d1d45504a585cc8b590e1eb76260597dc6c22f
Bonkook Koo <djmimi@nate.com>
2019-12-30 13:21:50 +0900

    Add sub menu

--- src/components/layout/header/NavBar.tsx
+++ src/components/layout/header/NavBar.tsx
@@ -1,115 +1,55 @@
-import React, { Component, SyntheticEvent } from 'react';
+import React, { Component } from 'react';
 
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-import { NavLink, RouteComponentProps } from 'react-router-dom';
-import { menuData } from '../../../constants/menuData';
-import IMenuItemData from '../../../constants/IMenuItemData';
-
-import styles from './NavBar.module.css';
-import { MenuState } from '../../../store/menu/types';
+import { Navbar, Nav } from 'react-bootstrap';
 import { connect } from 'react-redux';
+import { NavLink, RouteComponentProps } from 'react-router-dom';
+
+import { menuData } from '../../../constants/menuData';
+import { MenuState } from '../../../store/menu/types';
 import { AppState } from '../../../store';
-import { MenuId } from '../../../enums/MenuId';
+
+import SubMenu from './SubMenu';
 
 interface IProps {
   menu: MenuState;
 }
 
-interface IState {
-  isOpenDropdown: boolean;
-}
-
-class NavBar extends Component<IProps, IState> {
-  constructor(props: any) {
-    super(props);
-
-    console.log('Navbar 생성자');
-
-    this.state = { isOpenDropdown: false };
-  }
-
-  componentWillMount() {
-    console.log('componentWillMount');
-    const needToOpen = this.props.menu.selectedMenuId0 === MenuId.Product;
-    const { isOpenDropdown } = this.state;
-
-    if (needToOpen === isOpenDropdown) return;
-
-    this.setState({ isOpenDropdown: needToOpen });
-  }
-
-  onToggleDropdown = (
-    isOpen: boolean,
-    event: SyntheticEvent,
-    metadata: {
-      source: 'select' | 'click' | 'rootClose' | 'keydown';
-    }
-  ) => {
-    this.setState({ isOpenDropdown: isOpen });
-  };
-
-  getDropDownItems = (arrMenu: Array<IMenuItemData>) => {
-    return arrMenu.map(item => {
+class NavBar extends Component<IProps> {
+  getNavItems = () => {
+    return menuData.map(item => {
       return (
-        <NavDropdown.Item
+        <Nav.Link
           as={NavLink}
           key={item.id}
           to={item.link}
           className={
-            item.id === this.props.menu.selectedMenuId1
-              ? styles.selected
-              : undefined
+            item.id === this.props.menu.selectedMenuId0 ? 'selected' : undefined
           }
         >
           {item.text}
-        </NavDropdown.Item>
-      );
-    });
-  };
-
-  getNavItems = () => {
-    return menuData.map(item => {
-      if (!item.arrSub) {
-        return (
-          <Nav.Link
-            as={NavLink}
-            key={item.id}
-            to={item.link}
-            className={
-              item.id === this.props.menu.selectedMenuId0
-                ? 'selected'
-                : undefined
-            }
-          >
-            {item.text}
-          </Nav.Link>
-        );
-      }
-
-      return (
-        <NavDropdown
-          key={item.id}
-          title={item.text}
-          id="basic-nav-dropdown"
-          show={this.state.isOpenDropdown}
-          onToggle={this.onToggleDropdown}
-        >
-          {this.getDropDownItems(item.arrSub!)}
-        </NavDropdown>
+        </Nav.Link>
       );
     });
   };
 
   render() {
-    console.log('render');
-
     const navItems = this.getNavItems();
 
+    const { selectedMenuId0, selectedMenuId1 } = this.props.menu;
+
+    const selectedMenu0Data = menuData.find(
+      item => item.id === selectedMenuId0
+    );
+    const arrSub = selectedMenu0Data ? selectedMenu0Data.arrSub : null;
+
     return (
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="/">Home</Navbar.Brand>
-        <Nav className="mr-auto">{navItems}</Nav>
-      </Navbar>
+      <>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">Home</Navbar.Brand>
+          <Nav className="mr-auto">{navItems}</Nav>
+        </Navbar>
+        {arrSub && <SubMenu arrSub={arrSub} selectedId={selectedMenuId1!} />}
+      </>
     );
   }
 }
@@ -117,5 +57,4 @@ class NavBar extends Component<IProps, IState> {
 const mapStateToProps = (state: AppState) => ({
   menu: state.menu,
 });
-
 export default connect(mapStateToProps)(NavBar);
