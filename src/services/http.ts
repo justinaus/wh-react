d9d1ff3400d71ae61b093ef439f594fe4c3bf27e
justinaus <justinaus2k@gmail.com>
2020-03-04 18:47:09 +0900

    Update axios baseurl

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,13 +1,11 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
 const config: AxiosRequestConfig = {
-  baseURL: 'https://jsonplaceholder.typicode.com',
+  baseURL: '//jsonplaceholder.typicode.com',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json', 'X-Custom-Header': 'foobar' },
 };
 
 const http = axios.create(config);
-
-console.log('http');
 
 export default http;
