eb3ae4abade5bd0e06a02c989fb40cd7b518d9f7
justinaus <justinaus2k@gmail.com>
2020-03-12 16:49:27 +0900

    Remove containers

--- src/containers/ProgressbarContainer.ts
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import Progressbar from '../components/layout/progressbar/Progressbar';
-import { RootState } from '../store';
-
-const mapStateToProps = (state: RootState) => ({
-  progressbarState: state.progressbar,
-});
-
-export default connect(mapStateToProps)(Progressbar);
